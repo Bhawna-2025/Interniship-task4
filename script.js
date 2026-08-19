31284942b713a8f6f9a3c4a9d32ce743b4b032c3
CodeYourLifeWith-BHAWII <pbhawna2023@gmail.com>
2026-08-19 15:12:32 +0530

    build new more pages

--- script.js
+++ script.js
@@ -1,12 +1,21 @@
-let menubar = document.querySelector("#menubar")
-let hiddenMenu = document.querySelector("#hiddenMenu")
+let menubar = document.querySelector(".menubarIcon")
+let toggleMenu = document.querySelector(".toggleMenu")
+// let hiddenMenu3 = document.querySelector("#hiddenMenu")
 let cross = document.querySelector("#cross")
+
 menubar.addEventListener("click",()=>{
-    hiddenMenu.classList.toggle("translate-x-full");
-    hiddenMenu.classList.toggle("opacity-0");
+    toggleMenu.classList.toggle("translate-x-full");
+    toggleMenu.classList.toggle("opacity-0");
 })
 cross.addEventListener("click",()=>{
-    hiddenMenu.classList.toggle("translate-x-full")
-    hiddenMenu.classList.toggle("opacity-0");
+    toggleMenu.classList.toggle("translate-x-full")
+    toggleMenu.classList.toggle("opacity-0");
 })
 
+let menubar2 = document.querySelector("#menubar2")
+let navLinks = document.querySelector("#nav_links")
+
+menubar2.addEventListener("click",()=>{
+    navLinks.classList.toggle("translate-x-full")
+    navLinks.classList.toggle("opacity-0")
+})
