1c0dbc363c957e30a24b05c4279004f4145a46bb
CodeYourLifeWith-BHAWII <pbhawna2023@gmail.com>
2026-08-20 13:54:06 +0530

    Made all three pages responsive

--- script.js
+++ script.js
@@ -1,15 +1,25 @@
-let menubar = document.querySelector(".menubarIcon")
+let menubar_desktop = document.querySelector("#menubar_desktop")
+let menubar_mobile = document.querySelector("#menubar_mobile")
 let toggleMenu = document.querySelector(".toggleMenu")
-// let hiddenMenu3 = document.querySelector("#hiddenMenu")
 let cross = document.querySelector("#cross")
+let cross2= document.querySelector("#cross2")
+let nav_links= document.querySelector("#nav_links")
 
-menubar.addEventListener("click",()=>{
+menubar_desktop.addEventListener("click",()=>{
+    toggleMenu.classList.toggle("translate-x-full");
+    toggleMenu.classList.toggle("opacity-0");
+})
+menubar_mobile.addEventListener("click",()=>{
     toggleMenu.classList.toggle("translate-x-full");
     toggleMenu.classList.toggle("opacity-0");
 })
 cross.addEventListener("click",()=>{
     toggleMenu.classList.toggle("translate-x-full")
     toggleMenu.classList.toggle("opacity-0");
+})
+cross2.addEventListener("click",()=>{
+    nav_links.classList.toggle("translate-x-full")
+    nav_links.classList.toggle("opacity-0");
 })
 
 let menubar2 = document.querySelector("#menubar2")
